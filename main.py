f13460397a7868481009df3d4d28f077e59c3592
Lois <seijolois@gmail.com>
2025-11-23 01:49:16 +0100

    cors added

--- main.py
+++ main.py
@@ -10,6 +10,15 @@ load_dotenv()
 
 app = FastAPI()
 
+from fastapi.middleware.cors import CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/test")
 async def test_endpoint():
     """
